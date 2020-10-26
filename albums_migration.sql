b767ddd371d37b8bf1f5df795fcbcb1ed0a3ace2
Chelsea <chelsea.e.rolland@gmail.com>
2020-10-26 15:58:52 -0500

    completed the instrutor review

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,14 @@
+DROP DATABASE IF EXISTS codeup_test_db;
+CREATE DATABASE IF NOT EXISTS codeup_test_db;
 USE codeup_test_db;
+
 DROP TABLE IF EXISTS albums;
 CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR (20) NOT NULL,
-    name VARCHAR (30) NOT NULL,
-    release_date DATE,
-    sales DECIMAL(11,0),
+    artist VARCHAR (60) NOT NULL,
+    name VARCHAR (60) NOT NULL DEFAULT 'Untitled',
+    release_date INT UNSIGNED,
+    sales FLOAT UNSIGNED,
     genre VARCHAR (15),
     PRIMARY KEY (id)
 );
