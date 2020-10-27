17f3e0885e7d7182cf83e7876b4d9134f497091b
Chelsea <chelsea.e.rolland@gmail.com>
2020-10-27 14:49:17 -0500

    completed the instructor reviews

--- select_exercises.sql
+++ select_exercises.sql
@@ -4,19 +4,31 @@ SELECT '--------------------' AS 'Pink Floyd Albums';
 SELECT * FROM albums WHERE artist = 'Pink Floyd';
 
 SELECT '--------------------' AS 'Sgt. Pepper''s Release Date';
-SELECT * FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
 SELECT '--------------------' AS 'Nevermind';
-SELECT * FROM albums WHERE name = 'Nevermind';
+SELECT genre FROM albums WHERE name = 'Nevermind';
 
 SELECT '--------------------' AS '1990s Albums';
 SELECT * FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 
+SELECT '--------------------' AS '1990s Albums';
+SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+
 SELECT '--------------------' AS 'Less than 20mil Sales';
 SELECT * FROM albums WHERE sales < 20.00;
+
+SELECT '--------------------' AS 'Less than 20mil Sales';
+SELECT name FROM albums WHERE sales < 20;
 
 SELECT '--------------------' AS 'Rock Albums';
 SELECT * FROM albums WHERE genre = 'Rock';
 
+SELECT '--------------------' AS 'Rock Albums';
+SELECT name FROM albums WHERE genre = 'Rock';
+
 SELECT '--------------------' AS 'All Rock Albums';
 SELECT *  FROM albums WHERE genre LIKE '%Rock%';
+
+SELECT '--------------------' AS 'All Rock Albums';
+SELECT name FROM albums WHERE genre LIKE '%Rock%';
--- update_exercises.sql
+++ update_exercises.sql
@@ -1,33 +1,35 @@
 USE codeup_test_db;
 
 SELECT '--------------------' AS 'All Albums';
-SELECT * FROM albums WHERE id > 0;
+# Do not need the where statement since we are just selecting all of the entries and testing this now; you can leave off the where
+# SELECT * FROM albums WHERE id > 0;
+SELECT * FROM albums;
 
-UPDATE albums
-SET sales = sales * 10
-WHERE release_date > 1900;
+# Do not need the where statement since we are just selecting all of the entries and testing this now; you can leave off the where
+# UPDATE albums SET sales = sales * 10 WHERE id > 0;
+UPDATE albums SET sales = sales * 10;
 
 SELECT '--------------------' AS 'All Albums - UPDATED';
-SELECT * FROM albums WHERE id > 0;
+# SELECT * FROM albums WHERE id > 0;
+SELECT * FROM albums;
 
 
 SELECT '--------------------' AS 'Albums before 1980';
-SELECT * FROM albums WHERE release_date < 1980;
+SELECT * FROM albums WHERE release_date <= 1980;
 
-UPDATE albums
-SET release_date = release_date - 100
-WHERE release_date < 1980;
+UPDATE albums SET release_date = (release_date - 100) WHERE release_date <= 1980;
 
 SELECT '--------------------' AS 'Albums before 1980 - UPDATED';
-SELECT * FROM albums WHERE release_date < 1980;
+SELECT * FROM albums WHERE release_date <= 1980;
+
+SELECT '--------------------' AS 'Albums after 1980 - NO CHANGES';
+SELECT * FROM albums WHERE release_date > 1980;
 
 
 SELECT '--------------------' AS 'Michael Jackson Albums';
 SELECT * FROM albums WHERE artist = 'Michael Jackson';
 
-UPDATE albums
-SET artist = 'Peter Jackson'
-WHERE artist = 'Michael Jackson';
+UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
 
-SELECT '--------------------' AS 'Michael Jackson Albums - UPDATED';
+SELECT '--------------------' AS 'Michael/Peter Jackson Albums - UPDATED';
 SELECT * FROM albums WHERE artist = 'Peter Jackson';
